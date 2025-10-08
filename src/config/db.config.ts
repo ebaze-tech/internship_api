cfdd7c872b0722d6e3fb3fbf712b91452c63755b
ebaze-tech <ebaze.technologies@gmail.com>
2025-10-08 13:11:53 +0100

    Started the internship API. Done the auth, currently on user management

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,12 +1,12 @@
 import { Pool } from "pg";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    ssl: false
-})
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  ssl: { rejectUnauthorized: false },
+});
